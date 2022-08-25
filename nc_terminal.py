186cff7507678f0fe5ef3031f90eb2c16d28fb46
TomasCumlin <tomas.cumlin.1485@student.uu.se>
2022-08-25 09:28:42 +0200

    Instructions added to code

--- nc_terminal.py
+++ nc_terminal.py
@@ -4,9 +4,9 @@ import os
 import subprocess
 import sys
 
-infile=sys.argv[1]
-batch_name=sys.argv[2]
-output="output/"
+infile=sys.argv[1]      # The selected sequences to analyse in Geneious
+batch_name=sys.argv[2]  # Name of the batch. Can be modified in Geneious.
+output="/home/ete03/Documents/clinical_genomics/output/"   #Path to where your results should be saved.
 
 get_data=["/home/ete03/miniconda3/bin/nextclade", "dataset", "get", "--name=sars-cov-2", "--output-dir=data/sars-cov-2"]
 run_nextclade=["/home/ete03/miniconda3/bin/nextclade", "run", "--input-dataset", "data/sars-cov-2", "--output-all={}".format(output), "--output-tsv={}.tsv".format(batch_name), "--output-basename={}".format(batch_name), "{}".format(infile)]
