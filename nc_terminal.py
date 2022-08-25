4af106199ee4659b324fcea0c3970b3e70eb4845
TomasCumlin <tomas.cumlin.1485@student.uu.se>
2022-08-25 09:57:11 +0200

    Updated variable names

--- nc_terminal.py
+++ nc_terminal.py
@@ -11,10 +11,10 @@ output="/home/ete03/Documents/clinical_genomics/output/"   #Path to where your r
 get_data=["/home/ete03/miniconda3/bin/nextclade", "dataset", "get", "--name=sars-cov-2", "--output-dir=data/sars-cov-2"]
 run_nextclade=["/home/ete03/miniconda3/bin/nextclade", "run", "--input-dataset", "data/sars-cov-2", "--output-all={}".format(output), "--output-tsv={}.tsv".format(batch_name), "--output-basename={}".format(batch_name), "{}".format(infile)]
 
-output,error  = subprocess.Popen(
+output_dataset,error  = subprocess.Popen(
                     get_data, universal_newlines=True,
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
-output,error  = subprocess.Popen(
+output_results,error  = subprocess.Popen(
                     run_nextclade, universal_newlines=True,
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
