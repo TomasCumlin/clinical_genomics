2a2cab817b66bb344c60488bdd6cbc6450ac4b31
TomasCumlin <78538798+TomasCumlin@users.noreply.github.com>
2022-08-29 13:56:34 +0200

    Update nextclade_windows.bat

--- nextclade_windows.bat
+++ nextclade_windows.bat
@@ -1,6 +1,6 @@
 set input=%1
 set batch=%2
 
-nextclade dataset get --name sars-cov-2 --output-dir data/sars-cov-2
+C:\Users\KMB\nextclade dataset get --name sars-cov-2 --output-dir data/sars-cov-2
 
-nextclade run --input-dataset data/sars-cov-2 --output-all=cd --output-csv=%batch%.txt --output-basename=%batch% %input%
+C:\Users\KMB\nextclade run --input-dataset data/sars-cov-2 --output-all="C:\Users\KMB\TomasC\output" --output-csv=%batch%.txt --output-basename=%batch% %input%
