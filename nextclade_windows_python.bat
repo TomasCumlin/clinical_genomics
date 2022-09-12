62a949268c271cc2f2a3d5822abdd8470f39c801
TomasCumlin <78538798+TomasCumlin@users.noreply.github.com>
2022-09-12 11:54:17 +0200

    Update nextclade_windows_python.bat

--- nextclade_windows_python.bat
+++ nextclade_windows_python.bat
@@ -1,3 +1,6 @@
-:: To run nextclade_windows.py
+:: To run nextclade.py
+:: Update path to python location
+:: Update path to nextclade.py
+:: Ensure all paths in nextclade.py are correct
 
-C:\Users\KMB\AppData\Local\Programs\Python\Python310\python C:\Users\KMB\TomasC\nextclade_windows.py %1 %2
+C:\Users\KMB\AppData\Local\Programs\Python\Python310\python C:\Users\KMB\TomasC\nextclade.py %1 %2
